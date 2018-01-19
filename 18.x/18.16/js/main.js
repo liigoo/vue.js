35d7ab7f7598f1f31dcc3ab9b179eec04cd8623f
liigoo <1173818163@qq.com>
2018-01-19 17:53:59 +0800

    添加一处background

--- 18.x/18.16/js/main.js
+++ 18.x/18.16/js/main.js
@@ -44,7 +44,7 @@ Vue.component('popup',{
         '<button @click="toggle">popup</button>' +
 
         '<div v-if="visible">' +
-            '<span @click="hide">CLSOE</span>' +
+            '<span @click="hide" class="close">CLSOE</span>' +
             '<h4>title</h4>' +
             'Cleopatra Selene (died 69 BC) was a queen of Seleucid Syria (83–69 BC). The daughter of Ptolemy VIII and Cleopatra III of Egypt, she became the queen of Egypt in 115 BC when she was married to her brother, King Ptolemy IX, and later probably married King Ptolemy X. In 103 BC, Cleopatra III established an alliance with the Seleucid ruler Antiochus VIII; Cleopatra Selene was sent to be his bride, and stayed with him until his assassination in 96 BC. The widowed queen married her previous husband\'s brother, Antiochus IX, who died in 95 BC.' +
         '</div>' +
