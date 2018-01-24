6f2a995a3e9c1606e4951b02149144499f7275e4
liigoo <1173818163@qq.com>
2018-01-24 14:08:38 +0800

    vue.js 路由2种传参方式

--- 18.x/1.2/js/main.js
+++ 18.x/1.2/js/main.js
@@ -1,4 +1,5 @@
 //vue.js main
+//router 传参用两种方法
 var routes = [
     {
         path: '/',
@@ -18,7 +19,20 @@ var routes = [
             '</div>'
         },
 
-    }
+    },
+    {
+        /*url传参第一种方法是在path里面用冒号，语法是$route.params.自定义字段*/
+        /*url传参第二种方法其类型如aaa.com/aa?age=2,在url里面传参，语法是$route.query.age*/
+        path: '/user/:name',
+        component:{
+            template: '' +
+            '<div>' +
+            '   <h1>My name is {{$route.params.name}}</h1>' +
+            '我今年{{$route.query.age}}岁了' +
+            '</div>'
+        },
+
+    },
 ];
 
 var router = new VueRouter({
